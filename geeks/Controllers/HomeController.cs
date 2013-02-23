cc892972b875c6822d539fd220e71f7f3d4b376e
Julian Jelfs <julian.jelfs@gmail.com>
2013-02-23 21:22:00 +0000

    plumbed in raven db and got basic crud operations working for the event model

--- geeks/Controllers/HomeController.cs
+++ geeks/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using geeks.Models;
 
 namespace geeks.Controllers
 {
@@ -19,12 +20,45 @@ namespace geeks.Controllers
         }
 
         [Authorize]
+        public ActionResult Event(int id)
+        {
+            if (id > 0)
+            {
+                return View(RavenSession.Load<EventModel>(id));
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Event(EventModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //save the event
+                RavenSession.Store(model);
+                return RedirectToAction("Events");
+            }
+            return View();
+        }
+        
+        [Authorize]
+        public ActionResult DeleteEvent(int id)
+        {
+            var model = RavenSession.Load<EventModel>(id);
+            if (model != null)
+            {
+                RavenSession.Delete(model);
+            }
+            return RedirectToAction("Events");
+        }
+
+        [Authorize]
         public ActionResult Events()
         {
-            ViewBag.Message = "Create an event";
-
-
-            return View();
+            return View(from ev in RavenSession.Query<EventModel>()
+                        select ev);
         }
     }
 }
---
+++ geeks/Models/Event.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace geeks.Models
+{
+    public class EventModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+        
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+        
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Date and time")]
+        public DateTime Date { get; set; }
+        
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Venue")]
+        public string Venue { get; set; }
+    }
+}
